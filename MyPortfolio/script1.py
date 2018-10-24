f787a7d640a969aa9709667cf4978cf09bd1b3c8
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-10-24 23:37:58 +0530

    Add index method for landing page

--- MyPortfolio/script1.py
+++ MyPortfolio/script1.py
@@ -2,18 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	#return "Home page content goes here!!"
-	return render_template('home.html')
 
+@app.route('/')
+def index():
+	# return index page
+	return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+	# return "Home page content goes here!!"
+	return render_template('home.html')
 
 
 @app.route('/about/')
 def about():
-	#return "About us content goes here"
+	# return "About us content goes here"
 	return render_template('about.html')
-
 
 
 if __name__ == '__main__':
