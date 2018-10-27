ef1f37f76a25130f17b687be5b796deeebcb5967
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-10-27 23:46:23 +0530

    Add new method

--- MyPortfolio/script1.py
+++ MyPortfolio/script1.py
@@ -3,23 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def index_deprecated():
+# 	# return index page
+# 	title = "Indranil Ray - Full Stack Developer"
+# 	return render_template('index.html', title=title)
+
 @app.route('/')
 def index():
-	# return index page
-	title = "Indranil Ray - Full Stack Developer"
-	return render_template('index.html', title = title)
-
-
-@app.route('/home')
-def home():
-	# return "Home page content goes here!!"
-	return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
-	# return "About us content goes here"
-	return render_template('about.html')
+	return render_template('profile.html')
 
 
 if __name__ == '__main__':
